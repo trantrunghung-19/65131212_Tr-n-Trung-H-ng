d5e516521c680f763d95969869ba741526f3eaad
trantrunghung-19 <trantrunghung000@gmail.com>
2026-01-21 00:49:38 +0700

    Demo

---
+++ bai1/src/bai1/Chao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package bai1;
+
+/**
+ * 
+ */
+public class Chao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("XinChao");
+	}
+
+}
